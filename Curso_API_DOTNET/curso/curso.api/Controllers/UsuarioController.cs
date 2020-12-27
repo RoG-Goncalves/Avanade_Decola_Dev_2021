038f0bde08c8b2ee282dbc1ef11b173c65a01ce0
roG-Goncalves <rodrigo.oliveira.goncalves@gmail.com>
2020-12-27 00:23:26 -0300

    Adicionando código do curso

---
+++ Curso_API_DOTNET/curso/curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        //<summary>
+        //teste
+        //</sumary>
+
+
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModeInput loginViewModeInput)
+        {
+            return Ok(loginViewModeInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModeInput loginViewModeInput)
+        {
+            return Created("", loginViewModeInput);
+        }
+    }
+}
---
+++ Curso_API_DOTNET/curso/curso.api/Models/Usuarios/RegistroViewModeInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models.Usuarios
+{
+    public class RegistroViewModeInput
+    {
+        [Required(ErrorMessage = "O Login é Obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é Obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A Senha é Obrigatória")]
+        public string Senha { get; set; }
+
+
+
+    }
+}
